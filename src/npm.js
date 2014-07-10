cf3eed4edd083479e3b9875ae66b34a1cc04fc6e
Eugene Batalov <eabatalov89@gmail.com>
2014-07-11 01:11:37 +0400

    Implement package.json NPM file
    This file is be used to download all the Grunt dependencies
    required for the final build of target module.

--- src/npm.js
+++ src/npm.js
@@ -1,3 +1,27 @@
 function saveNPMFile(modName, modRepo) {
-    
+    var fs = require('fs');
+    var path = require('path');
+    var mod = modRepo.getModule(modName);
+
+    var packageObj = {
+        'name': mod.name,
+        'description': mod.description || 'no description',
+        'devDependencies': {
+            'grunt': 'latest',
+            'grunt-contrib-concat': 'latest',
+            'grunt-contrib-uglify': 'latest'
+        },
+        'repository': {
+            'type': 'file',
+            'url': 'file://' + mod.dirAbsPath
+        },
+        //This package file is only for usage in module development
+        'private': true
+    };
+    var packageObjJSON = JSON.stringify(packageObj, null, '    ');
+    var packageFilePath = path.join(mod.dirAbsPath, path.sep, 'package.json');
+    fs.writeFileSync(packageFilePath, packageObjJSON);
+
+    //console.log('Module ', mod.name, ' npm package file:');
+    //console.log(packageObjJSON);
 }
